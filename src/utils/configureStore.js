de9bab1636f88b75e484a472e1cf97d7f12a36d7
纳兰天忆 <103520146@qq.com>
2017-02-15 15:52:59 +0800

    工具集

--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -1,3 +1,25 @@
 /**
  * Created by nalantianyi on 2017/2/15.
  */
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+import {asyncMiddleware} from 'redux-amrc';
+
+let createStoreWithMiddleware;
+if (process.env.NODE_ENV === 'production') {
+    createStoreWithMiddleware = compose(applyMiddleware(thunk, asyncMiddleware))(createStore);
+} else {
+    createStoreWithMiddleware = compose(applyMiddleware(thunk, asyncMiddleware), typeof window === 'object'
+    && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtensions() : f => f)(createStore);
+}
+export default function configureStore(initialState) {
+    const store = createStoreWithMiddleware(rootReducer, initialState);
+    if (module.hot) {
+        module.hot.accept('../reducers', () => {
+            const nextRootReducer = require('../reducers/index').default;
+            store.replaceReducer(nextRootReducer);
+        })
+    }
+    return store;
+}
