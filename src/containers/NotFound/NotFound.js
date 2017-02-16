8fc29c290f1bba69e9ba91b5d6286a1ee504704e
纳兰天忆 <103520146@qq.com>
2017-02-16 10:41:32 +0800

    NotFound

---
+++ src/containers/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+/**
+ * Created by nalantianyi on 2017/2/16.
+ */
+import React from 'react';
+
+export default function NotFound() {
+    return (
+        <div className="container">
+            <h1>404!</h1>
+            <p>您要找的页面不存在！</p>
+        </div>
+    );
+}
