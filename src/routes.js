256f1f21f8cbc8b39085089efc9affd5686b5bad
纳兰天忆 <103520146@qq.com>
2017-02-16 11:14:44 +0800

    基础骨架

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,6 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import {Main, Home, NotFound} from './containers';
-import {loadCounter} from './actions/counter';
-import {loadStatistic} from './actions/statistic';
-import {loadAuthIfNeeded} from './actions/auth';
 
 const preload = promise => (nextState, replace, cb) => {
     if (__SERVER__ || nextState.location.action === 'PUSH') {
